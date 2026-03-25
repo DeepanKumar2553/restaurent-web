e5a576eb3d6b1d8703226b9e77fd33ecd5c8d325
DeepanKumar2553 <your-deepan-email@gmail.com>
2026-03-25 15:16:44 +0530

    final one

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 // This is the single source of truth for your API URL
 // If you deploy to the cloud later, you only change this one line.
-export const API_BASE_URL = "http://localhost:5050/api";
+export const API_BASE_URL = "https://restaurent-web-26sb.onrender.com";
